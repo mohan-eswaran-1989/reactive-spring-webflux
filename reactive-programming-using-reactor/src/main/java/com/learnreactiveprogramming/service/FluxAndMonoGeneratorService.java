dd1d6c258ac4ec36db9549eec6af232b66fe9184
Mohan Eswaran <mohaneswaran1989@gmail.com>
2023-01-03 23:02:59 -0600

    Implement the Reactive Programming using reactive types - Flux and Mono

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,26 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Mohan", "Priya", "Thuhira"));
+    }
+
+    public Mono<String> nameMono(){
+        return Mono.just("Mohan Eswaran");
+    }
+
+    public static void main(String[] args) {
+      FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+      fluxAndMonoGeneratorService.namesFlux().subscribe(name -> {
+          System.out.println("Flux Stream name is " + name);
+      });
+
+      fluxAndMonoGeneratorService.nameMono().subscribe(name -> System.out.println("Mono Stream name is " + name));
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,17 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
-
+    
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    @Test
+    void namesFlux() {
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        StepVerifier.create(namesFlux).expectNext("Mohan", "Priya", "Thuhira").verifyComplete();
+        StepVerifier.create(namesFlux).expectNextCount(3).verifyComplete();
+    }
 }
